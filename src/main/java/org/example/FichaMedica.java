b3324e0efda34fc10a241ae97f810dc4f7ea96cf
X-Camille <c.elgueta04@ufromail.cl>
2023-09-12 22:43:56 -0300

    Se desarrollaron las funcionalidades correspondientes para la clase FichaMedica, entre las que se incluyen opciones de si desea agregar diagnóstico o tratamiento del paciente, o si desea mostrar ficha médica.

---
+++ src/main/java/org/example/FichaMedica.java
@@ -0,0 +1,90 @@
+package org.example;
+
+import java.util.*;
+
+public class FichaMedica {
+    private String diagnostico = "";
+    private String tratamiento = "";
+    private final Propietario propietario;
+
+    public FichaMedica() {
+        propietario = new Propietario();
+    }
+
+    public void crearFichaMedica() {
+        agregarDatosUsuario(); // Métodos de la clase Propietario
+        agregarDatosAnimal(); // Métodos de la clase Animal
+        deseaAgregarDiagnostico();
+        deseaAgregarTratamiento();
+        mostrarFichaMedica();
+    }
+
+    public void deseaAgregarDiagnostico(){
+        Scanner scanner = new Scanner(System.in);
+        String opcion;
+        do {
+            System.out.print("¿Desea agregar un diagnóstico? 1. Sí - 2. No ");
+            opcion = scanner.nextLine();
+        } while(!opcion.equalsIgnoreCase("1") && !opcion.equalsIgnoreCase("2"));
+
+        switch (opcion) {
+            case "1":
+                diagnostico = agregarDiagnostico();
+                break;
+            case "2":
+                diagnostico = "No hay diagnóstico para este paciente.";
+                break;
+            default:
+                System.out.println("Surgió un problema al generar el diagnóstico."); // Caso por default en caso de que hubiera algún error
+        }
+    }
+
+    public void deseaAgregarTratamiento(){
+        Scanner scanner = new Scanner(System.in);
+        String opcion;
+        do {
+            System.out.print("¿Desea agregar un tratamiento? 1. Sí - 2. No ");
+            opcion = scanner.nextLine();
+        } while(!opcion.equalsIgnoreCase("1") && !opcion.equalsIgnoreCase("2"));
+
+        switch (opcion) {
+            case "1":
+                agregarTratamiento();
+                break;
+            case "2":
+                tratamiento = "No hay tratamiento para este paciente";
+                break;
+            default:
+                System.out.println("Surgió un problema al generar el tratamiento."); // Caso por default en caso de que hubiera algún error
+        }
+    }
+
+    public void agregarDatosUsuario() {
+        System.out.println("Ingrese los datos del usuario: ");
+        propietario.agregarDatos();
+    }
+
+    public void agregarDatosAnimal() {
+        System.out.println("Ingrese los datos del paciente: ");
+        // Métodos del objeto instanciado de la clase Animal
+    }
+
+    public String agregarDiagnostico() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Ingrese el diagnóstico del paciente: ");
+        return scanner.nextLine();
+    }
+
+    public void agregarTratamiento() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Ingrese el tratamiento del paciente: ");
+        tratamiento = scanner.nextLine();
+        scanner.close();
+    }
+
+    public void mostrarFichaMedica() {
+        System.out.println("El rut del propietario es " + propietario.getRut());
+        System.out.println(diagnostico);
+        System.out.println(tratamiento);
+    }
+}
