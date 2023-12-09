d576046841e7e1b76936f8854768400d37b4960f
miel <m.ferrada09@ufromail.cl>
2023-12-08 21:17:52 -0300

    Creación de clase Notificacion, con sus respectivos atributos (campos de texto, y fecha programada), así como método de verificación de fecha válida (para evitar el guardado de notificaciones pasadas)

---
+++ src/main/java/org/example/Notificacion.java
@@ -0,0 +1,30 @@
+package org.example;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+public class Notificacion {
+
+    private String encabezado, cuerpo;
+    private LocalDate fecha;
+
+    private LocalDate FechaValida(String fechaString){
+        DateTimeFormatter formato = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+        try {
+            LocalDate fecha = LocalDate.parse(fechaString, formato);
+            if (fecha.isBefore(LocalDate.now())) return null;
+            return fecha;
+        } catch (Exception e) {
+            System.err.println("Error al convertir la fecha: " + e.getMessage());
+            return null;
+        }
+    }
+
+    public Notificacion(String encabezado, String cuerpo, String fecha){
+        if (FechaValida(fecha) != null){
+            this.fecha = FechaValida(fecha);
+            this.cuerpo = cuerpo;
+            this.encabezado = encabezado;
+        } else throw new IllegalArgumentException("La fecha ingresada no es válida.");
+    }
+}
